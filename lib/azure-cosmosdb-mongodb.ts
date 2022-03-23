96b697cbfef82c06cf7d9b8f1866d8253cf41d82
Kevin Hill <kevinhill@live.com>
2022-03-23 14:17:28 -0700

    Adding todo method

---
+++ lib/azure-cosmosdb-mongodb.ts
@@ -0,0 +1,51 @@
+import { Schema, model, connect } from "mongoose";
+
+let db = null;
+
+const CategorySchema = new Schema(
+    { categoryName: String },
+    { timestamps: true }
+);
+const CategoryModel = model("Category", CategorySchema, "Bookstore");
+
+export const init = async () => {
+    if (!db) {
+        db = await connect(process.env["CosmosDbConnectionString"]);
+    }
+};
+export const addItem = async (doc) => {
+    const modelToInsert = new CategoryModel();
+    modelToInsert["categoryName"] = doc.name;
+
+    return await modelToInsert.save();
+};
+export const findItemById = async (id) => {
+    return await CategoryModel.findById(id);
+};
+export const findItems = async (query = {}) => {
+    return await CategoryModel.find({});
+};
+export const deleteItemById = async (id) => {
+    return await CategoryModel.findByIdAndDelete(id);
+};
+
+const TodoSchema = new Schema(
+    {
+        id: Number,
+        title: String,
+        created_date: Date,
+        done_date: Date,
+        status: String
+    },
+    { timestamps: true }
+)
+const TodoModel = model("Todo", TodoSchema, "Todo");
+export const addTodo = async (doc) => {
+    const modelToInsert = new TodoModel();
+    modelToInsert["title"] = doc.title;
+    modelToInsert["created_date"] = doc.created_date;
+    modelToInsert["done_date"] = doc.done_date;
+    modelToInsert["status"] = doc.status;
+
+    return await modelToInsert.save();
+}
---
+++ todo/index.ts
@@ -0,0 +1,42 @@
+import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import * as db from "../lib/azure-cosmosdb-mongodb"
+
+const httpTrigger: AzureFunction = async function (
+    context: Context,
+    req: HttpRequest
+): Promise<void> {
+
+    try {
+        let response = null;
+
+        await db.init();
+
+        switch (req.method) {
+            case "POST":
+                if (req.body.document) {
+                    const insertOneResponse = await db.addTodo(req.body.document);
+                    response = {
+                        documentResponse: insertOneResponse
+                    }
+                } else {
+                    throw Error("No document found");
+                }
+                break;
+            default:
+                throw Error(`${req.method} not allowed`)
+        }
+
+        context.res = {
+            body: response,
+        };
+    } catch (err) {
+        context.log(`*** Error throw: ${JSON.stringify(err)}`);
+
+        context.res = {
+            status: 500,
+            body: err
+        };
+    }
+};
+
+export default httpTrigger;
